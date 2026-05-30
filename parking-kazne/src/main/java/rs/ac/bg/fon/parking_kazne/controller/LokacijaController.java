ece41a54e7ae11ac605a5b7225404d1a597adbab
Lazar Ivosevic <lazzarivosevic@gmail.com>
2026-05-30 16:57:45 +0200

    fix response entity for class Lokacija

--- parking-kazne/src/main/java/rs/ac/bg/fon/parking_kazne/controller/LokacijaController.java
+++ parking-kazne/src/main/java/rs/ac/bg/fon/parking_kazne/controller/LokacijaController.java
@@ -3,6 +3,8 @@ package rs.ac.bg.fon.parking_kazne.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.bg.fon.parking_kazne.dto.LokacijaRequest;
 import rs.ac.bg.fon.parking_kazne.dto.LokacijaResponse;
@@ -18,25 +20,44 @@ public class LokacijaController {
     private final LokacijaService lokacijaService;
 
     @PostMapping
-    public LokacijaResponse create(@RequestBody @Valid LokacijaRequest lokacijaRequest){
+    public ResponseEntity<Object> create(@RequestBody @Valid LokacijaRequest lokacijaRequest){
         log.info("Creating location{}",lokacijaRequest);
-        System.out.println(lokacijaRequest);
-        return lokacijaService.create(lokacijaRequest);
+        try{
+            LokacijaResponse response=lokacijaService.create(lokacijaRequest);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
     @GetMapping
-    public List<LokacijaResponse> getAll(){
+    public ResponseEntity<Object> getAll(){
         log.info("Finding all locations");
-        return lokacijaService.findAll();
+        try{
+            List<LokacijaResponse>responses= lokacijaService.findAll();
+            return ResponseEntity.status(HttpStatus.OK).body(responses);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
     @GetMapping("/{id}")
-    public LokacijaResponse findById(@PathVariable Long id){
+    public ResponseEntity<Object> findById(@PathVariable Long id){
         log.info("Finding locatation with id: {}",id);
-        return lokacijaService.findById(id);
+        try{
+            LokacijaResponse response=lokacijaService.findById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id){
+    public ResponseEntity<Object> deleteById(@PathVariable Long id){
         log.info("Deleting location with id:{}",id);
-        lokacijaService.deleteById(id);
+        try{
+            lokacijaService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Successfully deleted");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 }
